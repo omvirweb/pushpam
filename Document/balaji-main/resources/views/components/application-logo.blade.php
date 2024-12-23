ca64ad525de1c48a4e340230c8a51c2cbbcb127f
Ubuntu <azureuser@Vm4.m2gh4hllttlenptkvmvdcy0z2h.rx.internal.cloudapp.net>
2024-12-23 18:55:54 +0000

    Server configurations

--- Document/balaji-main/resources/views/components/application-logo.blade.php
+++
@@ -1,2 +0,0 @@
-<img src="{{ asset('assets/dist/img/logo.png') }}" alt="Sanghavi Jewellers Logo"
-class="brand-image elevation-6" style="height: 45px;">
--- Document/balaji-main/resources/views/layouts/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer class="main-footer" style="text-align: center;">
-  <!-- To the right -->
-  <!-- Default to the left -->
-  <strong>Copyright &copy; 2014-2024 <a href="#">Balaji Trading</a>.</strong> All rights reserved.
-</footer>
